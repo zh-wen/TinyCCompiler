b066b0942032608920026de5d61c1ecb9654e1b1
温志豪 <zh.wen@hotmail.com>
2016-01-02 14:02:43 +0800

    编写Node

--- Node.cpp
+++ Node.cpp
@@ -4,8 +4,112 @@
  * @date    2016-01-02 10:55:14
  *
  **/
-#include'Node.h'
+#include"Node.h"
 
+Id::Id(Word word)
+{
+	variable = word;
+}
+
+void Id::createSTree(int depth, fstream file)
+{
+	printSpace(depth,file);
+	file << "id:" << variable.getLexeme() << endl;
+}
+
+string Id::toString()
+{
+	return variable.getLexeme();
+}
+
+Expr Id::reduce(SymbleT sbt,fstream file)
+{
+	return this;
+}
+
+Expr Id::genC(SymbleT sbt, fstream file)
+{
+	return this;
+}
+
+Logical::Logical(Operator op, Expr left, Expr right)
+{
+	this.oper = op;
+	this.left = left;
+	this.right = right;
+}
+
+void Logical::createSTree(int depth, fstream file)
+{
+	int subDepth = depth + 2;
+	printSpace(depth,file);
+	file << "op:" << oper.getOperator() << endl;
+	left.createSTree(subDepth,file);
+	right.createSTree(subDepth,file);
+}
+
+string Logical::toString()
+{
+	return string(left.toString() + " " + oper.toString() + " " + right.toString()); 
+}
+
+Expr Logical::reduce(SymbleT sbt,fstream file)
+{
+	this;
+}
+
+Expr Logical::genC(SymbleT sbt,fstream file)
+{
+	return new Logical(oper,left.reduce(sbt,file),right.reduce(sbt,file));
+}
+
+Temp::Temp(int index)
+{
+	this.index = index;
+}
+
+string Temp::toString()
+{
+	return "tmp_" + index;
+}
+
+Expr Temp::reduce(SymbleT sbt,fstream file)
+{
+	return this;
+}
+
+Expr Temp::genC(SymbleT sbt,fstream file)
+{
+	return this;
+}
+
+Constant::Constant(int token)
+{
+	this.mInt = token; 
+}
+
+void Constant::createSTree(int depth,fstream file)
+{
+	printSpace(depth,file);
+	file << "const:" <<mInt.getValue() << endl;
+}
+
+string Constant::toString()
+{
+	return string(itoa(mInt.getValue()));
+} 
+
+Expr reduce(SymbleT sbt,fstream file)
+{
+	return this;
+}
+
+Expr genC(SymbleT sbt,fstream file)
+{
+	return this;
+}
+
+Expr 
 Seq::Seq(Stmt left,Stmt right)
 {
 	this.left = left ;
@@ -81,4 +185,83 @@ void IfElse::genC(int depth,fstream file)
 	file << "}" << endl "else" << endl << "{" << endl;
 	elseStmt.genC(sbt,file);
 	file << "}" << endl;
+}
+
+Repeat::Reapeat(Logical logicalExpr,Seq stmt)
+{
+	this.logical = logicalExpr;
+	this.stmt = stmt;
+}
+
+void Repeat::createSTree(int depth,fstream file)
+{
+	int subDepth = depth + 2;
+	printSpace(depth,file);
+	file << "repeat" << endl;
+	stmt.createSTree(subDepth,file);
+	printSpace(depth,file);
+	file << "until" << endl;
+	logical.createSTree(subDepth,file);
+}
+
+void Repeat::genC(SymbleT sbt, fstream file)
+{
+	file << "do {" << endl;
+	stmt.genC(sbt,file);
+	Expr logicExpr = logical.genC(sbt,file);
+	file << "} while( !(" << logicExpr.toString() << "));" << endl;
+}
+
+Read::Read(Id id)
+{
+	this.id = id;
+}
+
+void Read::createSTree(int depth,fstream file)
+{
+	printSpace(depth,file);
+	file << "read:" << id.variable.getLexeme() << endl;
+}
+
+void Read::genC(SymbleT sbt, fstream file)
+{
+	file << "scanf(\"%\",&" id.variable.getLexeme() << ");" << endl;
+}
+
+Write::Write(Expr expr)
+{
+	this.expr = expr;
+}
+
+void Write::createSTree(int depth,fstream file)
+{
+	int subDepth = depth + 2;
+	printSpace(depth,file);
+	file << "write" << endl;
+	expr.createSTree(subDepth,file);
+}
+
+void Write::genC(SymbleT sbt, fstream file)
+{
+	Expr exprTemp = expr.gen(sbt,file).reduce(sbt,file);
+	file << "printf(\"%d\", ") << exprTemp.toString() << ");" << endl;
+}
+
+Assign::Assign(Id left,Expr expr)
+{
+	this.left = left;
+	this.expr = expr;
+}
+
+void Assign::createSTree(int depth,fstream file)
+{
+	int subDepth = depth + 2;
+	printSpace(depth,file);
+	file << "assign to:" << left.variable.getLexeme() << endl;
+	expr.createSTree(subDepth, file);
+}
+
+void Assign::genC(SymbleT sbt,fstream file)
+{
+	file << left.variable.getLexeme() << " = " << expr.genC(sbt,file).toString << endl;
 }
--- Node.h
+++ Node.h
@@ -8,6 +8,7 @@
 #ifndef NODE.H
 #define NODE.H
 #include<fstream>
+#incldue<string>
 using namespace std;
 
 //语法树节点基类
@@ -21,21 +22,106 @@ public:
 
 protected:
 	//用于生成语法树的缩进表示
-	static void print Space(int depth, fstream file)
+	static void printSpace(int depth, fstream file)
 	{
 		for (int i = 0;i < depth; i++)
 		{
-			out.print(' ');
+			file << ' ';
 		}
 	}	
+};
+
+//表达式节点
+class Expr : public Node
+{
+public:
+	 Expr();
+	~ Expr();
+	//表达式规约，将表达式规约为一个值
+	virtual Expr reduce(SymbolT sbt,fstream file);
+
+	//返回表达式
+	virtual Expr genC(SymbolT sbt,fstream file);
+};
+
+//标识符
+class Id : public Expr
+{
+protected:
+	Word variable;
+
+	void genC(SymbolT sbt,fstream file)
+	Expr reduce(SymbolT sbt,fstream file);
+
+public:
+	Id(Word variable);
+	~Id();
+	
+	string toString();
+	void creatSTree(int depth, fstream file);
+};
+
+//逻辑表达式
+class Logical : public Expr
+{
+private:
+	Operator oper;
+	Expr left,right;
+
+protected:
+	void genC(SymbolT sbt,fstream file)
+	Expr reduce(SymbolT sbt,fstream file);
+
+public:
+	Logical(Operator oper,Expr left,Expr right);
+	~Logical();
+	
+	string toString();
+	void creatSTree(int depth, fstream file);
+};
+
+
+//临时变量
+class Temp : public Expr
+{
+private:
+	int index;
+
+protected:
+	Expr reduce(SymbolT sbt,fstream file);
+	Expr genC(SymbolT sbt,fstream file);
+
+public:
+	Temp(int index);
+	~Temp();
+	
+	void creatSTree(int depth,fstream file);
+	string toString();	
+};
+
+class Constant : public Expr
+{
+private: 
+	int mInt;
+
+protected:
+	Expr reduce(SymbolT sbt,fstream file);
+	Expr genC(SymbolT sbt,fstream file);
+
+public:
+	Constant(int token);
+	~Constant();
+	
+	string toString();
+	void creatSTree(int depth, fstream file);
 };
 
 //语句节点
 class Stmt : public Node
 {
 public:
-	 Stmt();
-	~ Stmt();
+	Stmt();
+	~Stmt();
 	//生成C代码
 	virtual void genC(SymbolT sbt,fstream file)
 };
@@ -48,6 +134,7 @@ private:
 public:
 	Seq(Stmt left,Stmt right);
 	~Seq();
+
 	void creatSTree(int depth, fstream file);
 	void genC(SymbolT sbt,fstream file);
 };
@@ -84,8 +171,8 @@ private:
 	Seq thenStmt,elseStmt;
 
 public:
-	 IfElse(Logical logicalExpr,Seq thenStmt,Seq elseStmt);
-	~ IfElse();
+	IfElse(Logical logicalExpr,Seq thenStmt,Seq elseStmt);
+	~IfElse();
 	
 	void creatSTree(int depth, fstream file);
 	void genC(SymbolT sbt,fstream file);
@@ -98,8 +185,48 @@ private:
 	Logical logical;
 	Seq stmt;
 public:
-	 Repeat(Logical logical, Seq stmt);
-	~ Repeat();
+	Repeat(Logical logical, Seq stmt);
+	~Repeat();
+
 	void creatSTree(int depth, fstream file);
 	void genC(SymbolT sbt,fstream file);
 };
+
+class Read : public Stmt 	
+{
+private:
+	Id id;
+public:
+	Read(Id id);
+	~Read();
+
+	void creatSTree(int depth, fstream file);
+	void genC(SymbolT sbt,fstream file);	
+};
+
+class Write : public Stmt
+{
+private:
+	Expr expr;
+public:
+	Write(Expr expr);
+	~Write();
+
+	void creatSTree(int depth, fstream file);
+	void genC(SymbolT sbt,fstream file);	
+};
+
+class Assign : public Stmt
+{
+private:
+	Id left;
+	Expr expr;
+public:
+	Assign(Id left,Expr expr);
+	~Assign();
+	
+	void creatSTree(int depth, fstream file);
+	void genC(SymbolT sbt,fstream file);	
+};
+
+#endif
